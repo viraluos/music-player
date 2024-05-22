15ef20ac4f664f407dac659b571ebaa1b2f3d88e
1nsan3ly <xdgamingreal@gmail.com>
2024-05-22 13:50:45 +0200

    modificato:             src/components/Sidebar.tsx 	nuovo file:             src/hooks/useLocalStorage.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState, useRef } from "react";
 import Box from "@/components/Box";
 import Player from "@/components/Player";
 import Playlists from "@/components/Playlists";
+import useLocalStorage from "@/hooks/useLocalStorage";
 
 interface SidebarProps {
     children?: React.ReactNode;
@@ -13,9 +14,9 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({}) => {
     const [minWidth, maxWidth, defaultWidth] = [200, 1600, 400];
-    const sidebarwidth = parseInt(localStorage.getItem("sidebarWidth") || defaultWidth.toString())
+    const [value, setValue] = useLocalStorage("sidebarWidth", "");
 
-    const [width, setWidth] = useState( sidebarwidth  );
+    const [width, setWidth] = useState( value );
     const isResized = useRef(false);
 
     useEffect(() => {
@@ -24,7 +25,7 @@ const Sidebar: React.FC<SidebarProps> = ({}) => {
           return;
         }
 
-        setWidth((previousWidth) => {
+        setWidth((previousWidth: number) => {
           const newWidth = previousWidth + e.movementX;
           const isWidthInRange = newWidth >= minWidth && newWidth <= maxWidth;
           return isWidthInRange ? newWidth : previousWidth;
@@ -34,32 +35,42 @@ const Sidebar: React.FC<SidebarProps> = ({}) => {
       window.addEventListener("mouseup", () => {
         isResized.current = false;
       });
+
+      console.log("Storage Value: ", value)
+      setWidth(value);
     }, []);
 
     useEffect(() => {
-      localStorage.setItem("sidebarWidth", width.toString());
+      setValue(width);
     }, [width]);
+
+    if (!width)
+      return (<></>)
+
+    {
+      // component con tutto questo e gli passo la width
+    }
 
     return (
       <div style={{ width: `${width}px` }} className="w-full md:flex resize-x lg:w-fit">
 
-      <div className="flex flex-col w-full">
+        <div className="flex flex-col w-full">
 
-        <Box className="mb-2">
-          
-          <Player />
+          <Box className="mb-2">
 
-        </Box>
+            <Player />
 
-        <Box className="mb-2">
+          </Box>
 
-          <Playlists />
+          <Box className="mb-2">
 
-        </Box>
-        
-      </div>
+            <Playlists />
 
-        <div 
+          </Box>
+
+        </div>
+
+        <div
           className="w-2 h-full cursor-col-resize"
           onMouseDown={() => {
             isResized.current = true;
